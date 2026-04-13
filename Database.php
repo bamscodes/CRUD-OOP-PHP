38161150d2b22400adfaf829655a296423917ea8
Bambang Sugiarto <137086894+bamscodes@users.noreply.github.com>
2026-04-13 09:11:20 +0700

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "db_akademik";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            //Untuk menampilkan  error jika terjadi kesalahan query SQL, sehingga lebih mudah melakukan debugging.
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
